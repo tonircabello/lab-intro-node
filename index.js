44f06a97aa7dc63dc3e00964f494716ea363190b
tonircabello <tonircabello@gmail.com>
2024-05-10 23:30:01 +0200

    done

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items =[];
+    this.length = this.items.length; 
+  }
 
-  add(item) {}
 
-  get(pos) {}
 
-  max() {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b);
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
   sum() {}
 
